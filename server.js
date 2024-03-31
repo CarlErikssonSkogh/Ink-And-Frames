a60e7ed43e7792f90a1277d93e16377857272661
CarlErikssonSkogh <carl.erikssonskogh@elev.ga.ntig.se>
2024-03-31 14:57:15 +0200

    styled the header

--- server.js
+++ server.js
@@ -9,7 +9,6 @@ app.set('views', path.join(__dirname, 'frontend'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'hbs'); // Set Handlebars as the view engine
 dotenv.config({path: "./.env"});
-
 const db = mysql.createConnection({
     //värden hämtas från .env
     host: process.env.DATABASE_HOST,
@@ -38,6 +37,27 @@ app.get("/register", (req, res) => {
     res.render("register");
 });
 
+
+app.get('/search', (req, res) => {
+    const query = req.query.query; // Retrieve the search query from the query parameters
+    // Perform a search for movies based on the query
+    // Return search results to the client
+    const responseText = "Your search query was: " + query;
+    // Send the response as plain text
+    res.send(responseText);
+});
+
 app.listen(4000, ()=> {
     console.log("Servern körs, besök http://localhost:4000")
 })
+
+
+/*How to handle the search later
+
+
+app.get('/search', (req, res) => {
+    const query = req.query.query; // Retrieve the search query from the query parameters
+    // Perform a search for movies based on the query
+    // Return search results to the client
+});
+*/
